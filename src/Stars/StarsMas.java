4dce90579658dc6d713101b996a18208428decdd
HelenaLeonidovna <94039853+HelenaLeonidovna@users.noreply.github.com>
2021-12-10 12:22:26 +0200

    Initial commit

---
+++ src/Stars/StarsMas.java
@@ -0,0 +1,269 @@
+package Stars;
+
+public class StarsMas {
+
+    public static void One() {
+        int x, y;
+        for (x = 0; x < 7; x++) {
+            for (y = 0; y < 7; y++) {
+                System.out.print(" * ");
+            }
+            System.out.println(
+
+            );
+
+        }
+    }
+
+    public static void Two() {
+        String[][] array = new String[7][7];
+        array [0][0] = "****";
+        array [0][1] = "****";
+        array [0][2] = "****";
+        array [0][3] = "****";
+        array [0][4] = "****";
+        array [0][5] = "****";
+        array [0][6] = "****";
+        array [1][0] = "****";
+        array [1][6] = "****";
+        array [2][0] = "****";
+        array [2][6] = "****";
+        array [3][0] = "****";
+        array [3][6] = "****";
+        array [4][0] = "****";
+        array [4][6] = "****";
+        array [5][0] = "****";
+        array [5][6] = "****";
+        array [6][0] = "****";
+        array [6][1] = "****";
+        array [6][2] = "****";
+        array [6][3] = "****";
+        array [6][4] = "****";
+        array [6][5] = "****";
+        array [6][6] = "****";
+
+        int i, y;
+       for (i = 0; i < array.length; i++) {
+       for (y = 0; y < array[i].length; y++) {
+       System.out.print(array[i][y]+"\t");
+       }
+           System.out.println();
+       }
+
+            }
+
+
+    public static void Three() {
+        String[][] array = new String[7][7];
+        array [0][0] = "****";
+        array [0][1] = "****";
+        array [0][2] = "****";
+        array [0][3] = "****";
+        array [0][4] = "****";
+        array [0][5] = "****";
+        array [0][6] = "****";
+        array [1][0] = "****";
+        array [1][5] = "****";
+        array [2][0] = "****";
+        array [2][4] = "****";
+        array [3][0] = "****";
+        array [3][3] = "****";
+        array [4][0] = "****";
+        array [4][2] = "****";
+        array [5][0] = "****";
+        array [5][1] = "****";
+        array [6][0] = "****";
+
+        int i, y;
+        for (i = 0; i < array.length; i++) {
+            for (y = 0; y < array[i].length; y++) {
+                System.out.print(array[i][y]+"\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    public static void  Four() {
+        String[][] array = new String[7][7];
+        array [0][0] = "****";
+        array [1][0] = "****";
+        array [1][1] = "****";
+        array [2][0] = "****";
+        array [2][2] = "****";
+        array [3][0] = "****";
+        array [3][3] = "****";
+        array [4][0] = "****";
+        array [4][4] = "****";
+        array [5][0] = "****";
+        array [5][5] = "****";
+        array [6][0] = "****";
+        array [6][1] = "****";
+        array [6][2] = "****";
+        array [6][3] = "****";
+        array [6][4] = "****";
+        array [6][5] = "****";
+        array [6][6] = "****";
+
+
+        int i, y;
+        for (i = 0; i < array.length; i++) {
+            for (y = 0; y < array[i].length; y++) {
+                System.out.print(array[i][y]+"\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void  Five() {
+        String[][] array = new String[7][7];
+        array [0][6] = "****";
+        array [1][5] = "****";
+        array [1][6] = "****";
+        array [2][4] = "****";
+        array [2][6] = "****";
+        array [3][3] = "****";
+        array [3][6] = "****";
+        array [4][2] = "****";
+        array [4][6] = "****";
+      array [5][1] = "****";
+      array [5][6] = "****";
+       array [6][0] = "****";
+       array [6][1] = "****";
+       array [6][2] = "****";
+       array [6][3] = "****";
+       array [6][4] = "****";
+       array [6][5] = "****";
+       array [6][6] = "****";
+
+
+        int i, y;
+        for (i = 0; i < array.length; i++) {
+            for (y = 0; y < array[i].length; y++) {
+                System.out.print(array[i][y]+"\t");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void Six() {
+        String[][] array = new String[7][7];
+        array [0][0] = "****";
+        array [0][1] = "****";
+        array [0][2] = "****";
+        array [0][3] = "****";
+        array [0][4] = "****";
+        array [0][5] = "****";
+        array [0][6] = "****";
+        array [1][1] = "****";
+        array [1][6] = "****";
+        array [2][2] = "****";
+        array [2][6] = "****";
+        array [3][3] = "****";
+        array [3][6] = "****";
+        array [4][6] = "****";
+        array [4][4] = "****";
+        array [5][6] = "****";
+        array [5][5] = "****";
+        array [6][6] = "****";
+
+        int i, y;
+        for (i = 0; i < array.length; i++) {
+            for (y = 0; y < array[i].length; y++) {
+                System.out.print(array[i][y]+"\t");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void Seven() {
+        String[][] array = new String[7][7];
+        array [0][0] = "****";
+        array [0][6] = "****";
+        array [1][1] = "****";
+        array [1][5] = "****";
+        array [2][2] = "****";
+        array [2][4] = "****";
+        array [3][3] = "****";
+      array [4][2] = "****";
+      array [4][4] = "****";
+      array [5][1] = "****";
+      array [5][5] = "****";
+      array [6][0] = "****";
+      array [6][6] = "****";
+
+
+        int i, y;
+        for (i = 0; i < array.length; i++) {
+            for (y = 0; y < array[i].length; y++) {
+                System.out.print(array[i][y]+"\t");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+
+    public static void Eight() {
+        String[][] array = new String[7][7];
+        array [0][0] = "****";
+        array [0][1] = "****";
+        array [0][2] = "****";
+        array [0][3] = "****";
+        array [0][4] = "****";
+        array [0][5] = "****";
+        array [0][6] = "****";
+        array [1][1] = "****";
+        array [1][5] = "****";
+        array [2][2] = "****";
+        array [2][4] = "****";
+        array [3][3] = "****";
+
+        int i, y;
+        for (i = 0; i < array.length; i++) {
+            for (y = 0; y < array[i].length; y++) {
+                System.out.print(array[i][y]+"\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void  Nine() {
+        String[][] array = new String[7][7];
+        array[3][3] = "****";
+        array[4][2] = "****";
+        array[4][4] = "****";
+        array[5][1] = "****";
+        array[5][5] = "****";
+        array[6][0] = "****";
+        array[6][1] = "****";
+        array[6][2] = "****";
+        array[6][3] = "****";
+        array[6][4] = "****";
+        array[6][5] = "****";
+        array[6][6] = "****";
+
+
+        int i, y;
+        for (i = 0; i < array.length; i++) {
+            for (y = 0; y < array[i].length; y++) {
+                System.out.print(array[i][y] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import Stars.StarsMas;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StarsMas starsMas = new StarsMas();
+     StarsMas.One();
+        System.out.println();
+
+        //топ-8 самых костыльных решений ниже:
+
+     StarsMas.Two();
+        System.out.println();
+     StarsMas.Three();
+        System.out.println();
+     StarsMas.Four();
+        System.out.println();
+     StarsMas.Five();
+        System.out.println();
+     StarsMas.Six();
+        System.out.println();
+     StarsMas.Seven();
+        System.out.println();
+     StarsMas.Eight();
+        System.out.println();
+        StarsMas.Nine();
+        System.out.println();
+
+
+    }
+}
